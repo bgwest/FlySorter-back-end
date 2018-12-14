59ae6101e3072ac89b1a7d7d0dc3169761b7d645
Tom <tnorth93@gmail.com>
2018-12-14 13:16:35 -0800

    removed outdated subassembly get test

--- src/__test__/lib/subAssemblyMock.js
+++ src/__test__/lib/subAssemblyMock.js
@@ -2,19 +2,36 @@
 
 const faker = require('faker');
 const SubAssembly = require('../../model/sub-assembly');
+const partMock = require('./part-mock');
 
 const subAssemblyMock = module.exports = {};
 
 subAssemblyMock.pCreateSubAssemblyMock = () => {
-  return new SubAssembly({
-    subId: faker.lorem.words(1) + Math.random().toString(),
-    subPart: faker.lorem.words(1),
-    subVersion: faker.lorem.words(2),
-    subQuantity: Math.random().toString(),
-    subMinutes: Math.random().toString(),
-  }).save();
+  const resultMock = {};
+
+  return partMock.pCreatePartMock()
+    .then((createdPartMock) => {
+      resultMock.part = createdPartMock;
+
+      return new SubAssembly({
+        subId: faker.lorem.words(1) + Math.random().toString(),
+        subPart: faker.lorem.words(1),
+        subVersion: faker.lorem.words(2),
+        subQuantity: Math.random().toString(),
+        subMinutes: Math.random().toString(),
+        partIDRef: faker.lorem.words(1),
+        parts: createdPartMock._id,
+      }).save();
+    })
+    .then((createdSubAssemblyMock) => {
+      resultMock.subAssembly = createdSubAssemblyMock;
+      return resultMock;
+    });
 };
 
 subAssemblyMock.pCleanSubAssemblyMocks = () => {
-  return SubAssembly.remove({});
+  return Promise.all([
+    SubAssembly.remove({}),
+    partMock.pCleanPartMocks(),
+  ]);
 };
